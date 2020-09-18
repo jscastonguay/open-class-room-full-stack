99c623cdccbbf59e5ce0192256b491ac83ca9ce2
Jean-Sébastien Castonguay <jscastonguay@gmail.com>
2020-09-18 15:38:52 -0400

    Après avoir modfié le verbe PUT pour l'image.

--- routes/stuff.js
+++ routes/stuff.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 router.post('/', auth, multer, stuffCtrl.createThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 router.get('/', auth, stuffCtrl.getAllThings);
